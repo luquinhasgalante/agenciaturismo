ad484d36df8ff6b6ac9fe995300dcc840478c484
Lucas Milhão <lucasmilhao683@gmail.com>
2025-12-22 22:16:59 -0300

    Add files via upload

---
+++ AgenciaTurismo/src/src/Controle/CadHotelDAO.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controle;
+
+import Modelo.CadHotel;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Pichau
+ */
+public class CadHotelDAO {
+    Connection conexao;
+    
+    public CadHotelDAO() {
+        conexao = new ConexaoDAO().conectaBancoDados();
+    }
+    
+    private void criarTable() {
+        try{
+        String sql = "CREATE TABLE IF NOT EXISTS hoteis ( "
+                + "id int primary key not null auto_increment,"
+                + "nome varchar(60),"
+                + "cidade varchar(60),"
+                + "preco varchar(60),"
+                + "descricao varchar(250),"
+                + "estado varchar(60),"
+                + "imagem varchar(255)"
+                + ");";
+        
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.execute();
+        stmt.close();
+        
+        } catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, "Não foi possível criar tabela: " + e.getMessage());
+        }
+    }
+    
+    public void CadastrarHotel(CadHotel h) {
+        try{
+            criarTable();
+            
+            String sql = "INSERT INTO hoteis (nome, cidade, preco, descricao, estado, imagem) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, h.getNome());
+            stmt.setString(2, h.getCidade());
+            stmt.setDouble(3, h.getPreco());
+            stmt.setString(4, h.getDescricao());
+            stmt.setString(5, h.getEstado());
+            stmt.setString(6, h.getCaminhoImagem());
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Hotel cadastrado com sucesso!");
+        } catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar hotel: " + e.getMessage());
+        }
+        
+    }
+    
+    // READ (LISTAR)
+    public List<CadHotel> listarHoteis() {
+        List<CadHotel> lista = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM hoteis";
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                CadHotel h = new CadHotel();
+                h.setId(rs.getInt("id"));
+                h.setNome(rs.getString("nome"));
+                h.setCidade(rs.getString("cidade"));
+                h.setPreco(rs.getDouble("preco"));
+                h.setDescricao(rs.getString("descricao"));
+                h.setEstado(rs.getString("estado"));
+                h.setCaminhoImagem(rs.getString("imagem"));
+                lista.add(h);
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar usuários: " + erro.getMessage());
+        }
+        return lista;
+    }
+}
